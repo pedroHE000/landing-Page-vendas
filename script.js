ca01356c216187ca2c5cf661aa40802d47c6447c
pedro <pedrogeme4@gmail.com>
2024-01-26 12:29:20 -0300

    testes do site

---
+++ script.js
@@ -0,0 +1,16 @@
+// Função JavaScript para redirecionar para o link desejado
+function MiniConsoledeJogosRetroPortátilALIXPRESS() {
+    // Substitua "https://www.seulink.com" pelo link real
+    var link = "https://s.click.aliexpress.com/e/_Ddwjetx";
+    
+    // Redirecionar para o link
+    window.location.href = link;
+  }
+  //link da amazom
+  function MiniConsoledeJogosRetroPortátilALIXPRESS() {
+    // Substitua "https://www.seulink.com" pelo link real
+    var link = "https://s.click.aliexpress.com/e/_Ddwjetx";
+    
+    // Redirecionar para o link
+    window.location.href = link;
+  }
